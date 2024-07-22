cf17cd9bc9368d58e541cbe1bdc42555a1f72f0d
Logeshwaran123 <hlhilalogu@gmail.com>
2024-07-22 12:05:00 +0530

    Update User Info get updated

---
+++ backend/controllers/user-controller.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+
+const users = JSON.parse(fs.readFileSync(`./dev-data/users.json`));
+
+module.exports.getAllUsers = function (req, res) {
+  const users = JSON.parse(fs.readFileSync(`./dev-data/users.json`));
+  res.status(200).json({ status: "Success", data: users });
+};
+
+module.exports.deleteAllUsers = function (req, res) {
+  const users = JSON.parse(fs.readFileSync(`./dev-data/users.json`));
+  res.status(204).json({ status: "Success", message: "All Users Deleted" });
+};
+
+// GET DELETE UPDATE CREATE - USERS Route
+module.exports.getUser = function (req, res) {
+  const user = users.find((cur) => cur._uid == req.params.id);
+  res.status(200).json({ status: "Success", data: user });
+};
+
+module.exports.deleteUser = function (req, res) {
+  const user = users.find((cur) => cur._uid == req.params.id);
+  users.splice(users.indexOf(user), 1);
+  fs.writeFileSync(`./dev-data/users.json`, JSON.stringify(users));
+  res.status(200).json({ status: "Success", message: "User deleted" });
+};
+
+module.exports.updateUser = function (req, res) {
+  const user = users.find((cur) => cur._uid == req.params.id);
+  const reqData = req.body;
+  if (reqData.hasOwnProperty("_uid")) {
+    res
+      .status(400)
+      .json({ status: "Unsuccessful", message: "ID cannot be changed" });
+  } else {
+    const ind = users.indexOf(user);
+    for (const [key, value] of Object.entries(req.body)) {
+      user[key] = value;
+    }
+    users.splice(ind, 1, user);
+    console.log(users);
+    fs.writeFileSync("./dev-data/users.json", JSON.stringify(users));
+    res.status(200).json({ status: "Success", data: user });
+  }
+};
--- backend/routes/user-route.js
+++ backend/routes/user-route.js
@@ -2,4 +2,15 @@ const express = require("express");
 const userRoute = express.Router();
 const userControllers = require("../controllers/user-controller");
 
+userRoute
+  .route("/users")
+  .get((req, res) => userControllers.getAllUsers(req, res))
+  .delete((req, res) => userControllers.deleteAllUsers(req, res));
+
+userRoute
+  .route("/user/:id")
+  .get((req, res) => userControllers.getUser(req, res))
+  .delete((req, res) => userControllers.deleteUser(req, res))
+  .patch((req, res) => userControllers.updateUser(req, res));
+
 module.exports = userRoute;
